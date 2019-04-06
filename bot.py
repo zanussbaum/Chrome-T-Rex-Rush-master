d7199a1d4858a6f27ba125a73c2eeb07b359d844
Fabio Arthur Ebsi von Schelling Goldman <f.vonschellinggoldman@campus.tu-berlin.de>
2019-04-06 14:01:03 -0400

    Created ToDo list

---
+++ bot.py
@@ -0,0 +1,15 @@
+"""
+This will be the created bot stub which our genetic program will 'design'
+It has functions to respond to the on screen events
+It will be constantly calling event functions based on screen (update every 0.1 sec??)
+    --> Possible second variable parameter for mutation or crossover
+Once a certain threshold is reached (differs from event to event), it will trigger its action
+These functions are going to be our actual crossover as such hopefully maintaining good responses
+
+TODO:
+Write the stubs for every permutation of events and write the main runner
+Parallelize runners
+Seed original parameters
+Generate Population
+Write the genetic runner
+"""
