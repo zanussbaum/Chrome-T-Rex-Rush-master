d3fab6e66986b2d5110ce7ca40b65a3120a44d12
Fabio Arthur Ebsi von Schelling Goldman <f.vonschellinggoldman@campus.tu-berlin.de>
2019-04-16 22:00:06 -0400

    Implemented similarity for an Individual when compared to a reference

--- individual.py
+++ individual.py
@@ -1,14 +1,15 @@
 """
     TO DO STILL:
-    def fitness(self):
-        fitness approximation through similarity 
-
     def similarity(self, other):
+        --> These will be done in the main runner
         run two instances that are as different as possible 
         then compare with others to get an approximation of their fitness
+Something to be aware of is right now duck and jump are super different when calcing difference
 """
 import random
 import numpy
+
+
 class Individual:
     """Individual class with a strategy for each scenario 
 
@@ -25,7 +26,7 @@ class Individual:
         fitness: a value based on how well an individual would do in the game 
         size: size of strategy array 
     """
-    def __init__(self, size=5,arr=None):
+    def __init__(self, size=5, arr=None):
         """Creating a random initial individual
 
         Parameters
@@ -37,9 +38,11 @@ class Individual:
             2 - double cactus
             3 - high ptera
             4 - low ptera
+            TODO: Validate that this is exhaustive
 
         """
         self.size = size
+        self.fitness = 0
         if arr is None:
             self.strategy = numpy.array([random.randint(-5,5) for x in range(self.size)])
         else:
@@ -49,16 +52,16 @@ class Individual:
         """Private method that returns a copy of the individual
 
         Returns:
-            Copy of individual with same startegy  
+            Copy of individual with same stategy
         """
-        b = numpy.zeros(self,size, dtype=numpy.int64)
+        b = numpy.zeros(self.size, dtype=numpy.int64)
 
         numpy.copyto(b, self.strategy)
 
         return Individual(size=self.size, arr=b)
 
     def mutate(self):
-        """Mutates a single point of the strategy
+        """Mutates a single point of the strategy within the interval of -5 and +5
 
 
         Returns:
@@ -66,9 +69,9 @@ class Individual:
         """
         to_mutate = self._copy()
 
-        point = random.randint(0,self.size)
+        point = random.randint(0, self.size)
 
-        to_mutate.strategy[point] = random.randint(-5,5)
+        to_mutate.strategy[point] = random.randint(-5, 5)
 
         return to_mutate
 
@@ -81,13 +84,41 @@ class Individual:
         Returns:
             this_crossover, other_crossover: two new crossed individuals 
         """
-        other_crossover = other._copy()
+        other_crossover = other._copy() # Need to fix scoping
         this_crossover = self._copy()
 
-        point = random.randint(0,self.size)
+        point = random.randint(0, self.size)
 
         temp = this_crossover.strategy[point:]
         this_crossover.strategy[point:] = other_crossover[point:]
         other_crossover.strategy[point:] = temp
 
         return this_crossover, other_crossover
+
+    def fitness_approx(self, other):
+        """Take in a second individual and call private similarity function to
+        generate an approximate fitness
+
+        :param other: Referential Individual
+        :return:
+        """
+        self.fitness = other.fitness * self.similarity(other)
+
+    def similarity(self, other):
+        """
+        Private: gives the average difference in percentage between two individuals
+        :param other: the other individual to be compared to
+        :return:
+        """
+        diff = 0
+        for index in range(self.size):
+            diff += (self.strategy[index]/other.strategy[index])
+        diff /= self.size
+        if diff > 1: # Over 100% doesn't mean better
+            return 2 - diff
+        else:
+            return diff
+
+
+
+
